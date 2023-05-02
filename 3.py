2edd638a9f7f7411bc92ec16f7c3707ad22f2af9
Luiscadt <Luiscadt@gmail.com>
2023-05-02 00:26:06 -0400

    Historias Locas

---
+++ 3.py
@@ -0,0 +1,13 @@
+youtuber = "Luis Acevedo"
+
+print("Youtuber: " + youtuber)
+print("Youtuber: {}".format(youtuber))
+
+adj = input ("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+malib = f"computer programing is so {adj}! It makes me so excited all the because I can do it!  I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(malib)
